0747548ffdb1dba16326c85d3dd0444241020127
H0uss3m <hossem.benmustapha@gmail.com>
2019-08-29 17:07:54 +0100

    fix filter after setting reducers

--- Components/FlightList/FlightList.js
+++ Components/FlightList/FlightList.js
@@ -1,58 +1,55 @@
-import React, { Component } from 'react'
-import { View, StyleSheet, FlatList,Text } from 'react-native'
-import FlightItem from '../FlightItem/FlightItem'
+import React, { Component } from "react";
+import { View, StyleSheet, FlatList, Text } from "react-native";
+import FlightItem from "../FlightItem/FlightItem";
 // import { withNavigation } from 'react-navigation';
-import { connect } from 'react-redux'
-var _ = require('lodash')
-
+import { connect } from "react-redux";
+var _ = require("lodash");
 
 class FlightList extends Component {
-  
-  
   // loadData () {
   //   const action ={type:'SET_SHIPMENT_LIST',value:this.props.data}
   //   this.props.dispatch(action)
   //     // this.setState({
-  //     //     data: 
+  //     //     data:
   //     //     filtredData:
   //     //   })
   //     }
-      componentDidMount () {
-        // this.loadData()
-        console.log("******************reducers data**************",this.props)
-        
-        // console.log('componentDidMount',this.props.SearchOptions)
-      }
-      
-      componentDidUpdate () {
-        // console.log(' */*/*/* flightlist Data /*/*/*/*', this.props.data)
-        // console.log(' this.props.data', this.props.data)
-        
-        // console.log('*******Did Update SearchOptions*******Update',this.props.shipmentList)
-        // console.log('Did Update filtredData',this.props.filtredData.status)
-        // console.log('Did Update shipmentList',this.props.shipmentList)
-        // if (this.props.SearchOptions.length > 0) {
-          //   let filtredData = this.state.data.filter( el => this.props.SearchOptions.includes(el.status))
-          //   console.log('filtredData',filtredData)
-          //   this.setState({
-            //     filtredData
-            //   })
-            // }
-            // console.log('params',this.props.navigation.state.params)
-          }
-          render () {
-            let filtredData
-            if (this.props.SearchOptions.length > 0){
-              filtredData=this.props.data.filter(el => this.props.SearchOptions.includes(el.status))
-            }else{
+  componentDidMount () {
+    // this.loadData()
+    console.log('******************reducers data**************',this.props.data)
 
-             filtredData=this.props.data
-            }
-            return (
-              <View style={styles.flightList_container}>
-      {/* <Text>this.props.SearchOptions : {this.props.SearchOptions}</Text> */}
+    // console.log('componentDidMount',this.props.SearchOptions)
+  }
+
+  componentDidUpdate () {
+    // console.log(' */*/*/* flightlist Data /*/*/*/*', this.props.data)
+    // console.log(' this.props.data', this.props.data)
+    // console.log('*******Did Update SearchOptions*******Update',this.props.shipmentList)
+    // console.log('Did Update filtredData',this.props.filtredData.status)
+    // console.log('Did Update shipmentList',this.props.shipmentList)
+    // if (this.props.SearchOptions.length > 0) {
+    //   let filtredData = this.state.data.filter( el => this.props.SearchOptions.includes(el.status))
+    //   console.log('filtredData',filtredData)
+    //   this.setState({
+    //     filtredData
+    //   })
+    // }
+    // console.log('params',this.props.navigation.state.params)
+  }
+  render () {
+    let filtredData
+    if (this.props.SearchOptions.length > 0) {
+      filtredData = this.props.data.filter(el =>
+        this.props.SearchOptions.includes(el.status)
+      )
+    } else {
+      filtredData = this.props.data
+    }
+    return (
+      <View style={styles.flightList_container}>
+        <Text>this.props.SearchOptions : {this.props.SearchOptions}</Text>
         <FlatList
-          data={this.props.data}
+          data={filtredData}
           keyExtractor={(item, index) => item.id.toString()}
           renderItem={({ item }) => <FlightItem item={item} />}
         />
